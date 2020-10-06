312d9896c38fdf197cf608638aeebcd20c564bab
Sam <Sam@DESKTOP-0Q5GNER>
2020-10-05 22:58:11 -0700

    Step 1 of CSE 360 Assignment 1

---
+++ CSE360Assignment1/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,52 @@
+package cse360assignment02;
+
+/**
+ * 
+ * @author Sam
+ *
+ */
+public class AddingMachine {
+  private int total;
+  
+  /**
+   * <p>Constructor for Adding Machine class</p>
+   */
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  
+  /**
+   * <p>Gets Total</p>
+   * @return 0
+   */
+  public int getTotal () {
+    return 0;
+  }
+  
+  /**
+   * <p>Adds the value</p>
+   * @param value
+   */
+  public void add (int value) {
+  }
+
+  /**
+   * <p>Subtracts the value</p>
+   * @param value
+   */
+  public void subtract (int value) {
+  }
+
+  /**
+   * <p>Makes a string</p>
+   */
+  public String toString () {
+    return "";
+  }
+
+  /**
+   * <p>Clears memory</p>
+   */
+  public void clear() {
+  }
+}
